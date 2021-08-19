2c671935230745602fe81b0aadd94fe1c08034e1
Gisela Espinoza <ggarcia@ugb.edu.sv>
2021-08-19 09:11:59 -0600

    Proyecto ejemplo biblioteca de clases

---
+++ BibliotecaClases/CollectionExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+    public class CollectionExample
+    {
+        public static void SortedListCollection()
+        {
+            //definir el tamaño de la lista
+            Console.Write("Cuantos elementos agregara a la lista");
+            var elements = Convert.ToInt32(Console.ReadLine());
+
+            //declara arrays vacios
+            object[] key = new object[elements];
+            object[] value = new object[elements];
+
+            //declarar la lista
+            SortedList<object, object> srtList = new SortedList<object, object>();
+            int i = 0;
+
+            while (i < elements)
+            {
+                Console.WriteLine("Escriba el elemento a agregar {0}", i);
+
+                //llenar los arrays
+                Console.Write("Key: ");
+                key[i] = Console.ReadLine();
+                Console.Write("Value: ");
+                value[i] = Console.ReadLine();
+
+                //llenar la lista
+                srtList.Add(key[i], value[i]);
+
+                i++;
+            }
+
+            //mostrar elementos en la lista
+            Console.WriteLine("****ELEMENTOS EN LA LISTA****");
+            foreach (KeyValuePair<object, object> element in srtList)
+            {
+                Console.WriteLine("{0}: {1}", element.Key, element.Value);
+            }
+        }
+    }
+}
---
+++ UsoBiblioteca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaClases;
+
+namespace UsoBiblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //llamado al metodo SortedListCollection de la clase CollectionExample
+            //de nuestro ensamblado
+            CollectionExample.SortedListCollection();
+            Console.ReadKey();
+        }
+    }
+}
